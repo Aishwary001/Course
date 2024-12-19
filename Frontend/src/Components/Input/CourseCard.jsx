62ac4c0318778a2fb3d9730582e61671f0d30498
Aishwary001 <harshrajaparmar17@gmail.com>
2024-12-19 12:45:57 +0530

    All ready and setup without an error

---
+++ Frontend/src/Components/Input/CourseCard.jsx
@@ -0,0 +1,14 @@
+function CourseCard() {
+    return (
+      <div className=" w-[25rem] rounded-md">
+        <img 
+          src="../../../public/images/Web3.png" 
+          alt="Web Development Course" 
+          className="w-full h-auto rounded-md"
+        />
+      </div>
+    );
+  }
+  
+  export default CourseCard;
+  
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,13 +1,59 @@
-import React from 'react'
-import Navbar from '../Components/Navbar/Navbar'
+import React from "react";
+import Navbar from "../Components/Navbar/Navbar";
+import CourseCard from "../Components/Input/CourseCard";
 
 function Home() {
   return (
     <>
-       <Navbar/>
-       <div>Home</div>
+      <div className="min-h-screen">
+        <Navbar />
+        <div className="min-h-80 flex justify-center text-center items-center p-6">
+          <section className="tracking-tight">
+            <div className="flex justify-center">
+              <h1
+                className="text-7xl font-bold tracking-tight leading-none"
+                style={{ color: "#3770E5" }}
+              >
+                100xDevs,
+              </h1>
+              <h1 className="text-7xl font-bold">beacause</h1>
+            </div>
+
+            <h1 className="text-7xl font-bold">10x ain't enough!</h1>
+            <p className="text-xl mt-6">
+              A beginner-friendly plateform for mastering programming skills.
+            </p>
+            <div className="flex gap-2 justify-center mt-5">
+              <button className="bg-blue-500 px-4 py-2 rounded-md text-white hover:bg-blue-900 transition ease-in-out">
+                Explore Course
+              </button>
+              <button className="bg-blue-500 px-4 py-2 rounded-md text-white hover:bg-blue-900 transition ease-in-out">
+                Explore Notes
+              </button>
+            </div>
+          </section>
+        </div>
+        <div className="whitespace-nowrap gap-2 p-6 overflow-hidden flex">
+          <div className="flex gap-4 scroll-container">
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          </div>
+          <div className="flex gap-4 scroll-container">
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
