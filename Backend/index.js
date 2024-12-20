b8377fbfe9abcc4da6f834d3bc4d76050b0b6273
Aishwary001 <harshrajaparmar17@gmail.com>
2024-12-20 08:32:46 +0530

    Connetion to Database is secured

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,16 @@ const app = express();
 app.use(express.json());
 const port = process.env.port || 3000;
 
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(()=> console.log("Connected to mongoDB"))
+.catch((err)=> console.log("Could not connect tto mongoDB", err))
+
 app.get('/', (req, res) => {
     res.send("Hello World!")
 })
